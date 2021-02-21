665891de884eda09a5c623a3675d7cabe82782fd
marekboro <marek.borowicz83@gmail.com>
2021-02-21 12:07:53 +0000

    1st commit

---
+++ src/models/tutorial.model.ts
@@ -0,0 +1,8 @@
+export interface Tutorial {
+    name: string;
+    url: string;
+}
+
+// ACTIONS -=> 
+// - is a TYPE in the form of sting
+// - contains a PAYLOAD of data // not always necessary. 
