6da60c05de8bd92ea53c0e079496b248b09c5f38
marekboro <marek.borowicz83@gmail.com>
2021-02-21 20:36:44 +0000

    fixed the removal, have an isse with splicing from state array, worked around by copying state, splicing that copy and then overwriting the state with its spliced copy :(

--- src/app/reducers/tutorial.reducer.ts
+++ src/app/reducers/tutorial.reducer.ts
@@ -1,6 +1,7 @@
 import { Action}  from '@ngrx/store'
 import { Tutorial } from '../models/tutorial.model'
 import * as TutorialActions from '../actions/tutorial.actions'
+import { rendererTypeName } from '@angular/compiler';
 
 
 const initialState: Tutorial = {
@@ -10,13 +11,20 @@ const initialState: Tutorial = {
 
 export function reducer(state:Tutorial[] = [initialState], action:TutorialActions.Actions) {
     switch (action.type) {
-        case TutorialActions.ADD_TUTORIAL:
-            return [...state,action.payload];
+        case TutorialActions.ADD_TUTORIAL:{
+            return [...state,action.payload];}
         
-            case TutorialActions.REMOVE_TUTORIAL:
-            var newState = state.splice(action.payload,1);
-            return newState;
-            
+            case TutorialActions.REMOVE_TUTORIAL:{
+              
+                // state.splice(action.payload,1);
+                // return state;
+
+                var stateCopy = [...state]
+                stateCopy.splice(action.payload,1);
+                state = stateCopy; 
+                return state;
+            } 
+
         default:
             return state;
     }
